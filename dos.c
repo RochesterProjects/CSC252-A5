3a7a339dd3dc710a09b0cbc691dd1d38662bffdb
shoham shitrit <shohami.sh@gmail.com>
2019-12-08 20:26:16 -0500

    adding stuff

--- dos.c
+++ dos.c
@@ -1,6 +1,31 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
 #include <stdlib.h>
+
+
+void func(int sockfd) 
+{ 
+    char buff[80]; 
+    int n; 
+    for (;;) { 
+        bzero(buff, sizeof(buff)); 
+        printf("Enter the string : "); 
+        n = 0; 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+        write(sockfd, buff, sizeof(buff)); 
+        bzero(buff, sizeof(buff)); 
+        read(sockfd, buff, sizeof(buff)); 
+        printf("From Server : %s", buff); 
+        if ((strncmp(buff, "exit", 4)) == 0) { 
+            printf("Client Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
 
 int main(int argc, char *argv[]) {
   if(argc != 2) {
@@ -15,6 +40,37 @@ int main(int argc, char *argv[]) {
 	exit(1);
   }
 
-  /* construct request, connect to server at port, and send it */
 
+  /* construct request, connect to server at port, and send it */
+  int sockfd, connfd; 
+    struct sockaddr_in servaddr, cli; 
+  
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr("192.168.56.1"); 
+    servaddr.sin_port = htonl(port); 
+
+    // connect the client socket to server socket 
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+  
+    // function for chat 
+    func(sockfd); 
+  
+    // close the socket 
+    close(sockfd);
 }
